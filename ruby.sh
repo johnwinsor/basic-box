c36de7866a349d0aee0641f8b335094898464477
John Winsor <johnwinsor@Johns-iMac.local>
2015-10-17 15:43:10 -0700

    Added Rails

--- ruby.sh
+++ ruby.sh
@@ -6,6 +6,7 @@ sudo apt-get -y install make build-essential libssl-dev libreadline6-dev zlib1g-
 sudo apt-get -y install libxslt-dev libxml2-dev > /dev/null
 
 #### install rbenv and ruby-build
+echo 'Installing rbenv'
 git clone git://github.com/sstephenson/rbenv.git /home/vagrant/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> /home/vagrant/.zshrc
 echo 'eval "$(rbenv init -)"' >> /home/vagrant/.zshrc
@@ -14,6 +15,21 @@ git clone git://github.com/sstephenson/ruby-build.git /home/vagrant/.rbenv/plugi
 #### no rdoc for installed gems
 echo 'gem: --no-ri --no-rdoc' >> /home/vagrant/.gemrc
 
+#### Install Ruby vi rbenv
+export RBENV_ROOT="${HOME}/.rbenv"
+export PATH="${RBENV_ROOT}/bin:${PATH}"
+export PATH="${RBENV_ROOT}/shims:${PATH}"
 /home/vagrant/.rbenv/bin/rbenv install 2.2.3
 /home/vagrant/.rbenv/bin/rbenv global 2.2.3
-sudo gem install bundler --no-ri --no-rdoc
+gem install bundler --no-ri --no-rdoc
+/home/vagrant/.rbenv/bin/rbenv rehash
+
+#### Install Rails
+echo 'Installing Rails'
+sudo add-apt-repository -y ppa:chris-lea/node.js > /dev/null
+sudo apt-get -y update > /dev/null
+sudo apt-get -y install nodejs > /dev/null
+gem install rails -v 4.2.4
+/home/vagrant/.rbenv/bin/rbenv rehash
+
+sudo apt-get clean
