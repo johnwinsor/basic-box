e2f7d7081bdb9a81c8a1f262c49d457e614670b3
John Winsor <johnwinsor@gmail.com>
2015-10-20 19:12:57 -0700

    added synced folder and postgres provisioner

--- postgres.sh
+++ postgres.sh
@@ -2,3 +2,6 @@
 
 sudo apt-get -y install postgresql postgresql-contrib libpq-dev
 
+sudo sudo -u postgres psql -1 -c "CREATE USER julia WITH PASSWORD 'julia';"
+sudo sudo -u postgres psql -1 -c "ALTER USER julia WITH SUPERUSER;"
+
